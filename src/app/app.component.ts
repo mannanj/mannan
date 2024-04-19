bbfee8529105edf7949698c48ce1b6615ca12b12
Mannan Javid <mannanjavid@gmail.com>
2024-04-19 00:50:27 -0400

    Have scrolling set links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // Track elements scrolled.
+  @ViewChild('introDiv', {static: false}) private introDiv: ElementRef<HTMLDivElement>;
+  @ViewChild('aboutDiv', {static: false}) private aboutDiv: ElementRef<HTMLDivElement>;
+  @ViewChild('resumeDiv', {static: false}) private resumeDiv: ElementRef<HTMLDivElement>;
+  @ViewChild('contactDiv', {static: false}) private contactDiv: ElementRef<HTMLDivElement>;
+
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView(){
+    if (this.introDiv){
+      const rect = this.introDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if (topShown && bottomShown) {
+        this.setLink('home');
+      }
+    }
+    if (this.aboutDiv){
+      const rect = this.aboutDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if (topShown && bottomShown) {
+        this.setLink('about');
+      }
+    }
+    if (this.resumeDiv){
+      const rect = this.resumeDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+      const onePercentVisible = (
+        Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < 1 ||
+        Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < 1
+      )
+      if (!onePercentVisible) {
+        this.setLink('resume');
+      }
+    }
+    if (this.contactDiv){
+      const rect = this.contactDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if (topShown && bottomShown) {
+        this.setLink('contact');
+      }
+    }
+  }
+
   title = 'mannan';
   selectedLink: string = 'home';
   darkMode: boolean = true;
@@ -14,13 +61,6 @@ export class AppComponent {
     this.selectedLink = link;
   }
 
-  isVisible(result: any): void {
-    console.log('result', result);
-    // if (visible) {
-    //   console.log('target visible', target);
-    // }
-  }
-  
   scrollToSection(section: string): void {
     const elem = document.getElementById(section);
     if (elem) {
